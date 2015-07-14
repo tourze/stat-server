4980ff1071636641b86ee464fbf02774b55fcef2
luzhongpeng <1234qwqw>
2015-07-15 03:06:59 +0800

    delete Applications folder

--- start.php
+++ start.php
@@ -1,16 +1,47 @@
 <?php
 
+use stat\Bootstrap\StatProvider;
+use stat\Bootstrap\StatWorker;
 use Workerman\Worker;
+use Workerman\WebServer;
 
 require 'bootstrap.php';
 
-// 标记是全局启动
-define('GLOBAL_START', 1);
+// StatProvider
+$statistic_provider = new StatProvider("Text://0.0.0.0:55858");
+$statistic_provider->name = 'StatProvider';
 
-// 加载所有Applications/*/start.php，以便启动所有服务
-foreach(glob(__DIR__.'/Applications/*/start*.php') as $start_file)
+// StatWorker
+$statistic_worker = new StatWorker("Statistic://0.0.0.0:55656");
+$statistic_worker->transport = 'udp';
+$statistic_worker->name = 'StatWorker';
+
+// WebServer
+$web = new WebServer("http://0.0.0.0:55757");
+$web->name = 'StatisticWeb';
+$web->addRoot('stat.tourze.com', ROOT_PATH . 'web');
+
+// recv udp broadcast
+$udp_finder = new Worker("Text://0.0.0.0:55858");
+$udp_finder->name = 'StatisticFinder';
+$udp_finder->transport = 'udp';
+$udp_finder->onMessage = function ($connection, $data)
 {
-    require_once $start_file;
-}
+    $data = json_decode($data, true);
+    if (empty($data))
+    {
+        return false;
+    }
+
+    // 无法解析的包
+    if (empty($data['cmd']) || $data['cmd'] != 'REPORT_IP')
+    {
+        return false;
+    }
+
+    // response
+    return $connection->send(json_encode(['result' => 'ok']));
+};
+
 // 运行所有服务
 Worker::runAll();
--- web/_init.php
+++ web/_init.php
@@ -4,8 +4,6 @@ use tourze\Base\Config;
 
 require '../bootstrap.php';
 
-define('ST_ROOT', ROOT_PATH . 'Applications/Statistics');
-
 // 覆盖配置文件
 foreach(glob(Config::load('statServer')->get('configCachePath') . '*.php')  as $php_file)
 {
