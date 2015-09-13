10f1e65141de20b3623a82098bc999e83d12742b
tourze <dev@tourze.com>
2015-09-13 12:29:45 +0800

    修改默认端口

--- config/main.php
+++ config/main.php
@@ -1,7 +1,5 @@
 <?php
 
-use tourze\Base\Base;
-
 return [
 
     'component' => [
@@ -33,7 +31,7 @@ return [
             'count'          => 4, // 打开进程数
             'user'           => '', // 使用什么用户打开
             'reloadable'     => true, // 是否支持平滑重启
-            'socketName'     => 'http://0.0.0.0:8080', // 默认监听8080端口
+            'socketName'     => 'http://0.0.0.0:8088', // 默认监听8080端口
             'contextOptions' => [], // 上下文选项
             'siteList'       => [
                 'local.uc.tourze.com' => WEB_PATH,
